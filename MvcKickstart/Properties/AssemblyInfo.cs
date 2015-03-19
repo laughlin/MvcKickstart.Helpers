4b2bd930d2b08bd0cd853a1bfebb43c771b30992
Jon Knopp <jknopp@laughlin.com>
2015-03-19 12:18:14 -0500

    updated assembly

--- MvcKickstart/Properties/AssemblyInfo.cs
+++ MvcKickstart/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcKickstartHelpers")]
+[assembly: AssemblyTitle("MvcKickstart.Helpers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvcKickstartHelpers")]
+[assembly: AssemblyProduct("MvcKickstart.Helpers")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
